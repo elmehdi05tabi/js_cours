4c59c575ee4ba5278c08fc9b11f174d94e757de8
El Mehdi Tabi <tabimehdi0@gmail.com>
2025-05-02 10:14:26 +0100

    regular expression en js

---
+++ regular_expression/chalange.js
@@ -0,0 +1,18 @@
+/*
+  Regular Expression
+  - Challenge
+*/
+
+let url1 = 'elzero.org';
+let url2 = 'http://elzero.org';
+let url3 = 'https://elzero.org';
+let url4 = 'https://www.elzero.org';
+let url5 = 'https://www.elzero.org:8080/articles.php?id=100&cat=topics';
+let test = "https://elzero.org/javascript-2021-regular-expression-challenge/" ; 
+let re = /(https?:\/\/)?(www.)?\w{3,16}.((org|com|fr|ma):\d{4}\/\w+.\w{1,5}\?.+)?/ig;
+console.log(url1.match(re));
+console.log(url2.match(re));
+console.log(url3.match(re));
+console.log(url4.match(re));
+console.log(url5.match(re));
+console.log(re.test(test)) ;
